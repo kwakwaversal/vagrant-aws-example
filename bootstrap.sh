1bbbbd21edb9fecda819b5f740481925c5ef4793
Paul Williams <kwakwaversal@gmail.com>
2016-09-14 21:17:14 +0100

    Bootstrap and dummy HTML file.

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Fixes 'WARNING! Your environment specifies an invalid locale.' with the
+# Ubuntu Server 14.04 LTS (HVM), SSD Volume Type instance.
+sudo locale-gen en_GB
+sudo update-locale LANG=en_GB
+
+# Install
+sudo apt-get update
+sudo apt-get install -y apache2
+if ! [ -L /var/www ]; then
+  sudo rm -rf /var/www
+  sudo ln -fs /vagrant /var/www
+fi
